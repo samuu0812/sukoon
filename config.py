9de2e6c0f8d0a529427fc66fded07f6163b40fe4
samuu0812 <soul67836@gmail.com>
2024-10-17 21:23:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,8 +11,7 @@ API_ID = 22451487
 API_HASH = "9e7ac680cfc997528edae9676dc4baec"
 
 # Get your token from @BotFather on Telegram.
-BOT_TOKEN = "7994707404:AAFhZsAdV1z25QNEutEovVIAyyYqJ40bKiU
-"
+BOT_TOKEN = "7994707404:AAFhZsAdV1z25QNEutEovVIAyyYqJ40bKiU"
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = "mongodb+srv://sam834786:sam834786@cluster0.gnsow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
